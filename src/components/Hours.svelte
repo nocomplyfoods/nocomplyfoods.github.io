0b6254b02e171b96d50a2744fd93336703e3aca3
Russell Samora <russellsamora@gmail.com>
2024-03-19 14:30:45 -0400

    playing around

---
+++ src/components/Hours.svelte
@@ -0,0 +1,49 @@
+<script>
+	import { MapPin } from "lucide-svelte";
+</script>
+
+<div class="hours">
+	<h3>Hours</h3>
+	<ul>
+		<li>FRI: 5pm - 10pm</li>
+		<li>SAT: 5pm - 10pm</li>
+		<li>SUN: 9am - 12pm</li>
+	</ul>
+	<p>
+		<small>
+			<a
+				href="https://maps.app.goo.gl/nqZa8FxQR6jP5Ltm7"
+				target="_blank"
+				rel="noreferrer"
+			>
+				258 Stockbridge Rd<br />Great Barrington, MA</a
+			>
+		</small>
+	</p>
+</div>
+
+<style>
+	div {
+		padding: 16px;
+	}
+
+	ul {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		margin-top: 8px;
+	}
+
+	@media only screen and (min-width: 640px) {
+		div {
+			position: absolute;
+			top: 16px;
+			right: 16px;
+			text-align: right;
+		}
+
+		h3 {
+			text-align: right;
+		}
+	}
+</style>
--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { range } from "d3";
 	import { getContext } from "svelte";
+	import Hours from "$components/Hours.svelte";
 	import Menu from "$components/Menu.svelte";
 	import Footer from "$components/Footer.svelte";
 
@@ -9,71 +10,64 @@
 	const data = getContext("data");
 </script>
 
-<!-- <h1>No Comply Foods</h1> -->
+<h1>No<br />Comply<br />Foods</h1>
 
-<!-- <Menu></Menu> -->
-<!-- <div class="bg"></div>
-<div class="grid-container">
-	{#each items as item}
-		<div class="grid-item">
-			<div class="grid-content"></div>
-		</div>
-	{/each}
-</div> -->
+<Hours></Hours>
+
+<div class="coming">
+	<img
+		src="assets/images/burger.png"
+		alt="skateboarding burger"
+		aria="hidden"
+	/>
+	<h2>Coming very soon...</h2>
+</div>
 
 <Footer />
 
 <style>
-	.bg {
-		height: 100svh;
-		width: 100%;
-		background-image: url("assets/bg.png");
-		background-size: 50%;
-		opacity: 0.2;
-	}
-	.grid-container {
-		display: none;
-		grid-template-rows: repeat(3, 1fr);
-		grid-auto-flow: column;
-		grid-auto-columns: 1fr;
-		opacity: 0.25;
-		/* display: none; */
+	h1 {
+		padding: 16px;
+		/* text-align: center; */
 	}
 
-	.grid-item {
-		width: 100%;
-		padding-top: 100%; /* This maintains the aspect ratio to be a square */
-		position: relative;
-		border: 0.25vw solid var(--color-bg);
-		opacity: 1;
+	.coming {
+		padding: 16px;
+		margin-top: 16px;
+		text-align: center;
+		overflow: hidden;
 	}
 
-	.grid-item:nth-of-type(5n + 1) {
-		background: var(--color-yellow);
+	img {
+		display: block;
+		width: 70%;
+		max-width: 320px;
+		margin: 0 auto;
+		transform-origin: 20% 80%;
+		animation: slideAndRotate 2s ease-out forwards;
 	}
 
-	.grid-item:nth-of-type(5n + 2) {
-		background: var(--color-orange);
+	@media only screen and (min-width: 640px) {
+		.coming {
+			position: fixed;
+			top: 50%;
+			left: 50%;
+			margin: 0;
+			transform: translate(-50%, -50%);
+			text-align: center;
+			width: 100%;
+		}
 	}
 
-	.grid-item:nth-of-type(5n + 3) {
-		background: var(--color-pink);
-	}
-
-	.grid-item:nth-of-type(5n + 4) {
-		background: var(--color-brown);
-	}
-
-	.grid-item:nth-of-type(5n + 5) {
-		background: var(--color-gray);
-	}
-
-	.grid-content {
-		position: absolute;
-		top: 0;
-		right: 0;
-		bottom: 0;
-		left: 0;
-		/* Style your content here */
+	@keyframes slideAndRotate {
+		0% {
+			transform: translateX(-80vw) rotate(-10deg);
+		}
+		95% {
+			transform: translateX(0) rotate(-10deg);
+		}
+		100% {
+			transform: translateX(0) rotate(0deg);
+		}
 	}
 </style>
